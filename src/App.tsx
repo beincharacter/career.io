4dc024d85ef0fd3ba38e6d5229ccc7d9c207c481
shubhampal <shubhampalatwork@gmail.com>
2024-06-25 23:41:17 +0530

    added some dummy code for coffee that i'll delete after words

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,37 @@ import { CompanyDetail } from './pages/CompanyDetail';
 import Login from './pages/Login';
 import Signup from './pages/SignUp';
 import Dashboard from './pages/OrgDashboard';
+import MyComponent from './componants/abc';
+
+// const App: React.FC = () => {
+
+//   const location = useLocation();
+//   const authRoutes = ['/auth/login', '/auth/signup', '/dashboard/organization'];
+
+//   const hideHeaderFooter = authRoutes.includes(location.pathname);
+//   return (
+//     <>
+
+//       {!hideHeaderFooter && <Header />}
+//       <Routes>
+//         <Route index element={<LandingPage />} />
+//         <Route path='/auth/login' element={<Login />} />
+//         <Route path='/auth/signup' element={<Signup />} />
+//         <Route path='/dashboard/organization' element={<Dashboard />} />
+//         <Route path="/find-jobs" element={<FindJobsComponent />} />
+//         <Route path='/find-companies' Component={LandingPage} />
+//         <Route path='/find-jobs/job-123456' Component={JobDetailComponent} />
+//         <Route path='/company' Component={CompanyDetail} />
+//       </Routes>
+//       {!hideHeaderFooter && <Footer />}
+//     </>
+//   );
+// };
 
 const App: React.FC = () => {
-
-  const location = useLocation();
-  const authRoutes = ['/auth/login', '/auth/signup', '/dashboard/organization'];
-
-  const hideHeaderFooter = authRoutes.includes(location.pathname);
   return (
     <>
-
-      {!hideHeaderFooter && <Header />}
-      <Routes>
-        <Route index element={<LandingPage />} />
-        <Route path='/auth/login' element={<Login />} />
-        <Route path='/auth/signup' element={<Signup />} />
-        <Route path='/dashboard/organization' element={<Dashboard />} />
-        <Route path="/find-jobs" element={<FindJobsComponent />} />
-        <Route path='/find-companies' Component={LandingPage} />
-        <Route path='/find-jobs/job-123456' Component={JobDetailComponent} />
-        <Route path='/company' Component={CompanyDetail} />
-      </Routes>
-      {!hideHeaderFooter && <Footer />}
+    <MyComponent />
     </>
   );
 };
